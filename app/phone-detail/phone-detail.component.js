ac982210efc0a2142a03751168e9f298140aa08f
nghi <nguyenhuunghiceo@gmail.com>
2018-06-05 22:33:30 +0700

    https://trello.com/c/bbIMYszv
    price of phones

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -9,7 +9,7 @@ angular.
         self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
           self.setImage(phone.images[0]);
         });
-
+        
         self.setImage = function setImage(imageUrl) {
           self.mainImageUrl = imageUrl;
         };
